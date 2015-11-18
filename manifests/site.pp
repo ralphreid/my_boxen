05fe00f9e6b1594de59c54e1e21c6db04542ec2c
Ralph Reid <beresfordjunior@me.com>
2015-11-18 12:08:24 -0800

    removed tidy-markdown

--- manifests/site.pp
+++ manifests/site.pp
@@ -68,7 +68,6 @@ node default {
   atom::package { 'markdown-sort-list': }
   atom::package { 'markdown-preview-opener': }
   atom::package { 'markdown-table-formatter': }
-  atom::package { 'tidy-markdown': }
   atom::package { 'markdown-format': }
   atom::package { 'linter-markdown': }
   atom::package { 'markdown-scroll-sync': }
